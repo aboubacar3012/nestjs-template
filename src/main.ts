d8b7d29e678d878bdc56de0357534b4c43367f1c
Aboubacar <a.diallo3012@gmail.com>
2025-02-11 13:22:15 +0100

    Ajout du service Prisma, module Prisma et configuration de Swagger pour l'API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Median')
+    .setDescription('The Median API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  //! End Swagger setup
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
